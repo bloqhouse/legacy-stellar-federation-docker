9e0d392b2a13cc34ec0302bf20a32eb3dfb52ff1
phra <greensoncio@gmail.com>
2018-03-15 13:50:39 +0100

    fix: update federation url

--- federation/federation.cfg.sh
+++ federation/federation.cfg.sh
@@ -1,9 +1,9 @@
 echo port = ${FEDERATION_PORT}
-echo 
+echo
 echo [database]
 echo type = "\"${DB_TYPE}\""
 echo dsn = "\"${DB_URI}\""
-echo 
+echo
 echo [queries]
-echo federation = "\"SELECT '${ISSUER}' as id, friendly_id as memo, 'text' as memo_type FROM accounts WHERE friendly_id = ? AND ? = '${DOMAIN}'\""
+echo federation = "\"SELECT '${RECEIVING_ACCOUNT}' as id, friendly_id as memo, 'text' as memo_type FROM accounts WHERE friendly_id = ? AND ? = '${DOMAIN}'\""
 echo reverse-federation = "\"SELECT friendly_id, '' as domain FROM accounts WHERE ? = ''\""
